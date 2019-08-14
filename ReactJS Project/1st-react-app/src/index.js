b1559d889b50c6b58d5dd8b8de27975c4b142a4f
Kelsey Slim <thekelseyslim@gmail.com>
2019-08-14 11:57:54 -0400

    Update Board's render function Change status message when there's a winner

--- ReactJS Project/1st-react-app/src/index.js
+++ ReactJS Project/1st-react-app/src/index.js
@@ -40,7 +40,18 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+
+    if (winner) {
+      status = 'Winner: ' + winner;
+
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    }
 
     return (
       <div>
