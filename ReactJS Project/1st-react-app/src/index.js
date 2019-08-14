fbe8c3c07396ed2638eb94fed55cf0e60e12b62d
Kelsey Slim <thekelseyslim@gmail.com>
2019-08-14 11:28:13 -0400

    Add handleClick function to Board class
    Because it is the parent component

--- ReactJS Project/1st-react-app/src/index.js
+++ ReactJS Project/1st-react-app/src/index.js
@@ -5,28 +5,39 @@ import './index.css';
 // -------------- START ----------------- //
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       // Each of the squares is a "button" element.
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+    />);
   }
 
   render() {
