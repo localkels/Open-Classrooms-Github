d378fd5a4a6860ee145d2b625ef3fd5f2d1af4e9
Kelsey Slim <thekelseyslim@gmail.com>
2019-09-04 16:11:57 -0400

    Add ButtonList and NewButton components

--- ReactJS Project/calculator-app/src/App.js
+++ ReactJS Project/calculator-app/src/App.js
@@ -1,38 +1,70 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from './Button';
+
+
+function NewButton(props) {
+    return <li><button value={props.value} onClick={props.onClick}>{props.value}</button></li>
+}
+
+
+class ButtonList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonSymbols: ["+","–","x","÷","1","2","3","4","5","6","7","8","9","0"],
+
+    }
+  }
+
+    render(){
+      const buttons = this.state.buttonSymbols.map((buttonSymbol) =>
+          <NewButton key={buttonSymbol} value={buttonSymbol} onClick={this.props.callback}/>
+        );
+
+      return (
+            <ul>{buttons}</ul>
+          );
+    }
+
+}
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      buttonSymbols: [1,2,3,4,5,6,7,8,9,0,"+","–","X","÷"]
-
-    }
+    // this.calculate = this.calculate.bind(this);
   }
 
-  render(){
-    let buttonSymbols = this.state.buttonSymbols;
-    return (
-      this.state.buttonSymbols.map((i) => {
-        return <button id={buttonSymbols[i]} key={i}>{buttonSymbols[i]}</button>
-      })
+  calculate = (e) => {
+    console.log(e.target.value);
+    //  NEXT UP: working on the calculating part
+    // Get the VERY BASICS working first, then clean up later
+  }
 
-      // <button id={buttonSymbols[1]}>{buttonSymbols[1]}</button>
+  render() {
+    return (
+      <div>
+        <ButtonList callback={this.calculate}/>
+      </div>
     )
   }
 }
 
-class ButtonBlock extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-
-    }
-  }
-
-
-  render(){
-    return (<div>Hi</div>)
-  }
-}
+// class ButtonList extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//
+//     }
+//   }
+//
+//
+//   render(){
+//     return (
+//       <div>Hi</div>
+//     )
+//   }
+// }
 
 export default App;
---
+++ ReactJS Project/calculator-app/src/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Button extends React.Component {
+  // constructor() {
+  //   super()
+  //
+  // }
+
+  render() {
+    return <button
+      id="1"
+      value="1"
+      >1</button>
+  }
+}
+
+export default Button;
